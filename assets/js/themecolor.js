802576d682e263f6ea8b5ddfcdd6c0a8967a035f
Frankie Foo <foo_xianyang@jabil.com>
2021-04-11 22:38:53 +0800

    Better coloring

---
+++ assets/js/themecolor.js
@@ -0,0 +1,39 @@
+let setColorMode = function(colorMode) {
+    if(colorMode == "system" && window.matchMedia) {
+        colorMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark": "light";
+    }
+
+    let bodyElement = document.querySelector("body");
+    bodyElement.setAttribute("data-color-mode", colorMode);
+
+    window.localStorage.setItem("color-mode", colorMode);
+}
+
+let loadColorMode = function() {
+    myStorage = window.localStorage
+    return myStorage.getItem("color-mode");
+}
+
+var preferredColorMode = loadColorMode();
+if(preferredColorMode == null || preferredColorMode == "light") {
+    setColorMode("light");
+} else if (preferredColorMode == "dark") {
+    setColorMode("dark");
+}
+
+// set button mode
+let darkModeButton = document.querySelector("button#darkMode");
+let lightModeButton = document.querySelector("button#lightMode");
+let systemModeButton = document.querySelector("button#systemMode");
+
+darkModeButton.addEventListener("click", function(event) {
+    setColorMode("dark");
+});
+
+lightModeButton.addEventListener("click", function(event) {
+    setColorMode("light");
+})
+
+systemModeButton.addEventListener("click", function(event) {
+    setColorMode("system");
+})
